448458c88062925b1278664d4e6506a1a3a0ce10
Zhexuan Liu <zhexuanliu@eric-pro.local>
2017-03-05 14:59:25 -0800

    configured css path and use sscss for styling

--- cordova_app/scripts/vueify-build.js
+++ cordova_app/scripts/vueify-build.js
@@ -4,5 +4,6 @@ var vueify = require('vueify');
 
 browserify('www/js/main.js')
   .transform(vueify)
+  .plugin('vueify-extract-css', { out: 'www/css/bundle.css' })
   .bundle()
   .pipe(fs.createWriteStream('www/js/bundle.js'))
--- cordova_app/www/js/app.vue
+++ cordova_app/www/js/app.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="app">      
-    <h1>Test</h1>
-    <p>{{test}}</p>
-  </div>
+		<div class="app">      
+			<h1>Test</h1>
+			<p>{{test}}</p>
+		</div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      test: 'Hello World!'
-    }
-  },
+	data () {
+		return {
+		test: 'Hello World!'
+		}
+  	},
 }
 </script>
+
+<style lang="scss">
+	.a {
+		width: "100%",
+	}
+</style>
