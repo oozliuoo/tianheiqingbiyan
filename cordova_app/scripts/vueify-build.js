00815c53675f74ce54e844d053088405c3c3c906
Zhexuan Liu <zhexuanliu@eric-pro.local>
2017-03-05 18:25:36 -0800

    reconfigure env to use vue-cli and cordova

--- cordova_app/scripts/vueify-build.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var browserify = require('browserify');
-var vueify = require('vueify');
-
-browserify('www/js/main.js')
-  .transform(vueify)
-  .plugin('vueify-extract-css', { out: 'www/css/bundle.css' })
-  .bundle()
-  .pipe(fs.createWriteStream('www/js/bundle.js'))
