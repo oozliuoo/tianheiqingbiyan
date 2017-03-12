b04c94b95369eedac453bbb15606f3515c173b85
Zhexuan Liu <zhexuanliu@eric-pro.local>
2017-03-11 16:44:49 -0800

    home page prototype completed

--- app/vueproject/config/index.js
+++ app/vueproject/config/index.js
@@ -3,11 +3,11 @@ var path = require('path')
 
 module.exports = {
   build: {
-    env: require('./prod.env'),
+    env: require('./dev.env'),
     index: path.resolve(__dirname, '../../www/index.html'),
     assetsRoot: path.resolve(__dirname, '../../www/'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
