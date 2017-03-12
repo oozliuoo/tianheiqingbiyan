6e627f8a91aabfbbcbb4bbc16e5e6910d7291def
Zhexuan Liu <zhexuanliu@eric-pro.local>
2017-03-11 16:54:50 -0800

    push to clean up repo

--- app/vueproject/src/router/index.js
+++ app/vueproject/src/router/index.js
@@ -9,7 +9,12 @@ export default new Router({
 		{
 			path: '/',
 			name: 'Home',
-			component: Home
-		}
+			component: Home,
+		},
+		{
+			path: '/create-room/',
+			name: "CreateRoom",
+			component: CreateRoom,
+		},
 	]
 })
